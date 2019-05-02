ec3397f928fd8a56ba22c55e06b02755f352884d
sonsoleslp <sonsoleslp@hotmail.com>
2019-05-02 15:17:49 +0200

    Adapt queries for pg

--- controllers/analytics_controller.js
+++ controllers/analytics_controller.js
@@ -258,15 +258,15 @@ exports.ranking = (req, res, next) => {
 
     if (isPg) {
         options.attributes = [...options.attributes,
-         [Sequelize.literal('STRING_AGG("retos->retosSuperados"."createdAt"," ")'), 'ca'],
-         [Sequelize.literal('STRING_AGG("retos->retosSuperados"."updatedAt"," ")'), 'ua'],
-         [Sequelize.literal('STRING_AGG("retos->retosSuperados"."puzzleId"," ")'), 'pi'],
-         [Sequelize.literal('STRING_AGG("retos->retosSuperados"."teamId"," ")'), 'ti']
+         [Sequelize.literal(`STRING_AGG("retos->retosSuperados"."createdAt",'')`), 'ca'],
+         [Sequelize.literal(`STRING_AGG("retos->retosSuperados"."updatedAt",'')`), 'ua'],
+         [Sequelize.literal(`STRING_AGG("retos->retosSuperados"."puzzleId",'')`), 'pi'],
+         [Sequelize.literal(`STRING_AGG("retos->retosSuperados"."teamId",'')`), 'ti']
 
-         [Sequelize.literal('STRING_AGG("teamMembers->members"."createdAt"," ")'), 'tca'],
-         [Sequelize.literal('STRING_AGG("teamMembers->members"."updatedAt"," ")'), 'tua'],
-         [Sequelize.literal('STRING_AGG("teamMembers->members"."teamId"," ")'), 'tti'],
-         [Sequelize.literal('STRING_AGG("teamMembers->members"."userId"," ")'), 'tui'],
+         [Sequelize.literal(`STRING_AGG("teamMembers->members"."createdAt",'')`), 'tca'],
+         [Sequelize.literal(`STRING_AGG("teamMembers->members"."updatedAt",'')`), 'tua'],
+         [Sequelize.literal(`STRING_AGG("teamMembers->members"."teamId",'')`), 'tti'],
+         [Sequelize.literal(`STRING_AGG("teamMembers->members"."userId",'')`), 'tui'],
         ]
     }
     if (turnId) {
